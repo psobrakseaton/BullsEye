df8b922ada0b3b055bd2569a9094232c1e39ad76
Patch Sobrak-Seaton <28573875+psobrakseaton@users.noreply.github.com>
2018-12-28 12:46:04 +0100

    Add files via upload
    Fully completed app from the Ray Wenderlich tutorial: Your First iOS App

---
+++ BullsEye/BullsEye/AboutViewController.swift
@@ -0,0 +1,31 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by Patch Sobrak-Seaton on 12/27/18.
+//  Copyright © 2018 Good Reason Co. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class AboutViewController: UIViewController {
+    
+    @IBOutlet weak var webView: WKWebView!
+
+    override func viewDidLoad() {
+      super.viewDidLoad()
+        
+        if let htmlPath = Bundle.main.path(forResource: "BullsEye", ofType: "html"){
+            let url = URL(fileURLWithPath: htmlPath)
+            let request = URLRequest(url: url)
+            webView.load(request)
+        }
+
+    }
+    
+    @IBAction func close(){
+        dismiss(animated: true, completion: nil)
+    }
+
+}
---
+++ BullsEye/BullsEye/AuthorViewController.swift
@@ -0,0 +1,22 @@
+//
+//  AuthorViewController.swift
+//  BullsEye
+//
+//  Created by Patch Sobrak-Seaton on 12/27/18.
+//  Copyright © 2018 Good Reason Co. All rights reserved.
+//
+
+import UIKit
+
+class AuthorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func close(){
+        dismiss(animated: true, completion: nil)
+    }
+}
